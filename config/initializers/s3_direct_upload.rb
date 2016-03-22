9f464da1283c9fca59f681dbd99cf22d4bbe7859
dardania84 <dardania84@gmail.com>
2016-03-22 03:13:14 -0400

    Update s3_direct_upload.rb

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -1,14 +1,14 @@
 if ENV['S3_ENABLED']
   S3DirectUpload.config do |c|
-    c.access_key_id = ENV['AKIAIYF5FJM52LGQWDAA']
-    c.secret_access_key = ENV['WrmJ3Ywkg4g0UNGt37MY5yhEv']
+    c.access_key_id = ENV['AKIAJNLDSDVYTTRM2WBA']
+    c.secret_access_key = ENV['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']
     c.bucket = ENV['dardania']
     c.region = ENV['US Standard']
   end
 
   # Used for debugging
   $s3_client = S3::Service.new(
-    access_key_id: ENV['AKIAIYF5FJM52LGQWDAA'],
-    secret_access_key: ENV['WrmJ3Ywkg4g0UNGt37MY5yhEv']
+    access_key_id: ENV['AKIAJNLDSDVYTTRM2WBA'],
+    secret_access_key: ENV['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']
   )
 end
